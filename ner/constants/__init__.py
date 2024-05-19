9994528d739c3fba7a6b1381174d17926c15417a
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2024-05-19 17:17:17 +0530

    epochs changed

--- ner/constants/__init__.py
+++ ner/constants/__init__.py
@@ -34,7 +34,7 @@ UNIQUE_LABELS_FILE_NAME = "unique_labels.pkl"
 
 MODEL_TRAINING_ARTIFACTS_DIR = "ModelTrainingArtifacts"
 LEARNING_RATE = 5e-3
-EPOCHS = 3
+EPOCHS = 1
 BATCH_SIZE = 2
 BERT_MODEL_INSTANCE_NAME = "bert_model_instance.pt"
 TOKENIZER_FILE_NAME = "tokenizer.pkl"
